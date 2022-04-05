903f1eac9b3faac658b050f92289b506bfbbea6c
obed004 <100470457+obed004@users.noreply.github.com>
2022-04-05 13:09:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var b = 50;
-/*console.log(b);
+console.log(b);
 document.writeln(b)
 document.getElementById("hello").innerHTML = "hello world";
 for (var i = 10; i<=50; i+=2){
     document.writeln(i)
     
-}*/
+}
 
 const  sum=[10 ,12, 14,16 ,18 ,20 ,22 ,24 ,26 ,28 ,30 ,32 ,34 ,36 ,38 ,40 ,42 ,44 ,46 ,48 ,50].reduce(function(result,item){
     return result+item;},0)
