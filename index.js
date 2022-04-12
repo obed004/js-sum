b577ef5256693ffac4ee4423e9d8a111b3e5a7a7
obed004 <100470457+obed004@users.noreply.github.com>
2022-04-12 07:03:36 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-/*var b = 50;
+var b = 50;
 console.log(b);
-document.writeln(b)
 document.getElementById("hello").innerHTML = "hello world";
-for (var i = 10; i<=50; i+=2){
-    document.writeln(i)
-    
-}*/
-//sum of all numbers 
-const  sum=[10 ,12, 14,16 ,18 ,20 ,22 ,24 ,26 ,28 ,30 ,32 ,34 ,36 ,38 ,40 ,42 ,44 ,46 ,48 ,50].reduce(function(result,item){
-    return result+item;},0)
-    document.writeln(sum)
+for (var i = 10; i<50; i++)
+{
+    if(i % 2==0){
+        document.writeln(i)
+    b += i
+   }
+};
+document.writeln(b)
+
+//sum of variables
